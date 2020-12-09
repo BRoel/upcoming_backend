484fb9d19282d6cb9fad836bcd145b27b07a5e4a
Brennan Roel <brennanroel@gmail.com>
2020-12-08 20:50:41 -0600

    added games controller index and create

---
+++ app/controllers/api/v1/games_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::GamesController < ApplicationController
+    
+    def index
+        games = Game.all
+        render json: games
+    end
+
+    def create
+        game = Game.new(game_params)
+        if game.save
+            render json: game, status: :accepted #sending status codes and accepting or rejecting (look more into this)
+        else
+            render json: {errors: game.errors.full_messages}, status: :unprocessible_entity
+    end
+
+    private
+
+    def game_params
+        params.require(:game).permit(:title, :description, :image_url, :genre_id)
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :games, only: [:index]
+      resources :games, only: [:index, :create]
     end
-  end 
+  end
 
 end
